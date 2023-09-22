4309e0d37b9be5bcbe0fe0dc2723bf4ce4b95e04
lucasfv <lucasfvera@hotmail.com>
2023-09-22 18:22:37 -0300

    Fix container module federation remote app name
    There was an issue when trying to fetch the remote file from the
    home remote app.
    We changes the remote app name in the webpack file but we forgot to
    change it in the container app when consuming it.
    
    This commit fixes the issue by updating the container's app remote
    name to homeApp.

--- container_react/modulefederation.config.js
+++ container_react/modulefederation.config.js
@@ -3,7 +3,7 @@ const { dependencies } = require('./package.json');
 module.exports = {
 	name: 'host',
 	remotes: {
-		home: 'home@http://localhost:3002/remoteEntry.js',
+		home: 'homeApp@http://localhost:3002/remoteEntry.js',
 		posts: 'posts@http://localhost:3003/_next/static/chunks/remoteEntry.js',
 	},
 	shared: {
