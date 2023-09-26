12d46293c69d7a1f5ca249d77ffbce3a39d47a51
lucasfv <lucasfvera@hotmail.com>
2023-09-25 22:55:25 -0300

    Add nested pages to posts app
    This commit adds two missing pages to the app to demonstrate a simple
    nested pages scenario.

---
+++ posts_next/pages/posts/first-post/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+import utilStyles from '../../../styles/utils.module.css';
+
+export default function Home() {
+	return (
+		<>
+			<Head>
+				<title>Pages app</title>
+			</Head>
+			<section className={utilStyles.headingMd}>
+				<p>This is the first post page</p>
+			</section>
+		</>
+	);
+}
