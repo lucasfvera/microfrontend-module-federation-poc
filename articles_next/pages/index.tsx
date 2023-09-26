4e0c8c3fdba93389c34bb94f7e4398fb47a8fd1c
lucasfv <lucasfvera@hotmail.com>
2023-09-25 22:56:18 -0300

    Render whole posts page in articles app
    This commit adds the exposed page from the Posts app to the Articles
    app to test rendering an entire page in a Nextjs host application.

--- articles_next/pages/index.tsx
+++ articles_next/pages/index.tsx
@@ -1,4 +1,5 @@
 import dynamic from 'next/dynamic';
+import Head from 'next/head';
 import { lazy } from 'react';
 
 // const Card = typeof window
@@ -11,13 +12,20 @@ import { lazy } from 'react';
 const Card = dynamic(() => import('homeApp/Card'), {
 	ssr: false,
 });
-const Banner = dynamic(() => import('posts/BannerNext'));
+const Banner = dynamic(() => import('posts/BannerNext'), { ssr: false });
+const PostsPage = dynamic(() => import('posts/pages/index'), { ssr: false });
 
 export default function Articles() {
 	return (
-		<main>
-			<Card />
-			<Banner />
-		</main>
+		<>
+			<Head>
+				<title>Articles Next app</title>
+			</Head>
+			<main>
+				<Card />
+				<Banner />
+				<PostsPage />
+			</main>
+		</>
 	);
 }
